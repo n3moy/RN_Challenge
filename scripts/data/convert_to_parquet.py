23041a188701c9a8f330cc6b775a45e1dc58e5d9
Vladislav <vladveprev29@gmail.com>
2022-09-22 23:10:48 +0300

    Add convert_to_parquet.py script, works slowly

---
+++ scripts/data/convert_to_parquet.py
@@ -0,0 +1,34 @@
+import os
+import py7zr
+# import pyarrow
+import pandas as pd
+
+ZIP_PATH = "../../data/archive/train_dataset.7z"
+OUT_PATH = "../../data/processed"
+
+
+def zip_to_parquet(zip_path: str, output_path: str = None):
+
+    with py7zr.SevenZipFile(zip_path, "r") as z_file:
+        filenames = z_file.getnames()
+        selective_files = [f for f in filenames if "processed" in f and ".csv" in f]
+        print(f"Amount of files to extract: {len(selective_files)}")
+
+        for ix, file_path in enumerate(selective_files):
+            # print(z_file.read(file_path).items())
+            res = z_file.read(file_path).items()
+            file_name = os.path.basename(file_path)
+            new_name = file_name.replace(".csv", ".parquet")
+            for fname, bio in res:
+                csv_file = pd.read_csv(bio, low_memory=False)
+                save_path = os.path.join(output_path, new_name)
+                # print(f"Saved to {save_path}")
+                csv_file.to_parquet(save_path)
+
+            z_file.reset()
+            if ix == 1000:
+                break
+
+
+if __name__ == "__main__":
+    zip_to_parquet(ZIP_PATH, OUT_PATH)
